6f9314b1b68cf1d36672282866f11b1d7668d20a
Merve Sarı <mervesari0597@gmail.com>
2022-10-12 22:44:42 +0300

    Second Class

---
+++ oop1/src/oop1/Main.java
@@ -0,0 +1,65 @@
+package oop1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product product1 = new Product();
+		product1.setName("Delonghi Kahve Makinesi");
+		product1.setDiscount(7);
+		product1.setUnitPrice(7500);
+		product1.setUnitsInStock(3);
+		product1.setImageUrl("image.jpg");
+				/*set value
+		product1.name = "Delonghi Kahve Makinesi";
+		product1.unitPrice = 7500;
+		product1.discount = 7;
+		product1.unitsInStock = 3;
+		product1.imageUrl = "bilmemne.jpg";*/
+		
+				//get
+		//System.out.println(product1.name);
+		
+		Product product2 = new Product();
+		product2.setName("Smeg Kahve Makinesi");
+		product2.setDiscount(7);
+		product2.setUnitPrice(7500);
+		product2.setUnitsInStock(3);
+		product2.setImageUrl("image.jpg");
+		
+		Product product3 = new Product();
+		product3.setName("Kitchen Kahve Makinesi");
+		product3.setDiscount(7);
+		product3.setUnitPrice(7500);
+		product3.setUnitsInStock(3);
+		product3.setImageUrl("image.jpg");
+		
+		Product[] products = {product1, product2, product3};
+		
+		System.out.println("<ul>");
+		for (Product product : products) {
+			System.out.println("<li>" +product.getName()+ "</li>");
+		}
+		System.out.println("</ul>");
+		
+		IndividualCustomer individualCustomer = new IndividualCustomer();
+		individualCustomer.setId(1);
+		individualCustomer.setPhone("05222222222");
+		individualCustomer.setCustomerNumber("12345");
+		individualCustomer.setFirstName("Merve");
+		individualCustomer.setLastName("Sarı");
+		
+		System.out.println(individualCustomer.getId());
+		
+		CorporateCustomer corporateCustomer = new CorporateCustomer();
+		corporateCustomer.setId(2);
+		corporateCustomer.setCompanyName("Kodlama.io");
+		corporateCustomer.setPhone("05323333333");
+		corporateCustomer.setTaxNumber("1111111111");
+		corporateCustomer.setCustomerNumber("54321");
+		
+		Customer[] customers = {individualCustomer, corporateCustomer};
+		
+		
+	}
+
+}
